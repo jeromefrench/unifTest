aeda3bc5ff0a9ea104c55663f9cd262178a10725
jerome <jerome.chardin@live.fr>
2020-05-24 14:37:42 +0200

    install vuex persistant

--- unif-test/src/store/index.js
+++ unif-test/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import productions from './modules/productions'
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex)
 
@@ -13,5 +14,6 @@ export default new Vuex.Store({
   },
   modules: {
   	  productions
-  }
+  },
+  plugins: [createPersistedState()]
 })
