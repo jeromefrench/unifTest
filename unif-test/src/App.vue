b24230d7fe33522c7d4cee838155925e8cf36d75
jerome <jerome.chardin@live.fr>
2020-05-24 11:49:27 +0200

    basic layout for all page

--- unif-test/src/App.vue
+++ unif-test/src/App.vue
@@ -1,34 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/list-productions">List Productions</router-link> |
-      <router-link to="/launch-production">Launch Production</router-link> |
-      <router-link to="/list-actors">List Actors</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav" >
+			<router-link to="/">Home</router-link> |
+			<router-link to="/list-productions">List Productions</router-link> |
+			<router-link to="/launch-production">Launch Production</router-link> |
+			<router-link to="/list-actors">List Actors</router-link>
+		</div>
+
+
+		<div class="columns">
+			<div class="column gutter"></div>
+			<div class="column is-three-quarters">
+				<router-view/>
+			</div>
+			<div class="column gutter"></div>
+		</div>
+
+
+
+	</div>
 </template>
 
 <style>
+
+.gutter {
+	background-color: hsl(0, 0%, 96%);
+	height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
+	background-color: hsl(0, 0%, 96%);
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	font-weight: bold;
+	color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
